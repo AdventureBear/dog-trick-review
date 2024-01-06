629afed7715fe013679b5614ec8e7ef7be3249c6
Suzanne Atkinson <suzanneatkinson@gmail.com>
2024-01-05 21:20:53 -0500

    first commit

---
+++ src/Trick.jsx
@@ -0,0 +1,12 @@
+function Trick({title, description}) {
+
+   return (
+       <div  className='card'>
+           <h2>{title}</h2>
+           <p>Category: Basic Obedience</p>
+           <p>{description}</p>
+       </div>
+   )
+}
+
+export default Trick
